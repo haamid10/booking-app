41f40568075faa5b857e17fbd2cd6af5dc40eead
haamid10 <apdixamiid919@gmail.com>
2023-11-26 15:28:00 +0300

    solved the error

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import express from 'express';
+const cors= require('cors');
+const  express= require('express');
 
 const app = express();
 app.use(express.json());
 
 app.use(cors({
     credentials: true,
-    origin: 'http://127.0.0.1:5173'
+    // origin: 'http://127.0.0.1:5173'
 }));
 
 
 
-app.get('/test', (req, res) => {
-    res.json("hello we are here");
-});
+// app.get('/test', (req, res) => {
+//     res.json("hello we are here");
+// });
 app.post('/register', (req,res) => {
-    const {name,email,password} =req.body;
-    res.json({name,email,password},"added")
+    const {name,password,email} =req.body;
+    res.json({name,password,email})
+    res.json("working")
 });
 
 app.listen(5000, () => console.log(`server running on port: http://localhost:5000`));
