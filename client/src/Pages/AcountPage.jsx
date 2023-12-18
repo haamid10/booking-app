a71553b45e1ac7e8e0ca169d78f38c3edb4b8d31
haamid10 <apdixamiid919@gmail.com>
2023-12-18 11:33:32 +0300

    writed this one in my own

--- client/src/Pages/AcountPage.jsx
+++ client/src/Pages/AcountPage.jsx
@@ -3,6 +3,7 @@ import { UserContext } from './../UserContext';
 import { Link, Navigate, useParams } from 'react-router-dom';
 
 const AcountPage = () => {
+
     const {ready,user}= useContext(UserContext);
     if(!ready){
       return 'loading...';
@@ -10,13 +11,18 @@ const AcountPage = () => {
     if(ready && !user) return <Navigate to='/login' />
       
      const {subpage} = useParams();
-      console.log(subpage);
+
+     const linkClasses =(type =null)=> {
+        const baseClasses = 'py-2 px-6 ';
+        const activeClasses = 'bg-primary text-white rounded-full';
+        return type === subpage ||subpage ===undefined && type ==='profile'  ? `${baseClasses} ${activeClasses}` : baseClasses;
+     }
     return (
     <div>
       <nav className='w-full m-8 gap-2 justify-center flex'>
-        <Link className='py-2 px-6 bg-primary text-white rounded-full' to={'/account'}>Profile</Link>
-        <Link className='py-2 px-6 ' to={'/account/bookings'}>My booking</Link>
-        <Link className='py-2 px-6' to={'/account/Places'}>My places</Link>
+        <Link className={linkClasses('profile')} to={'/account'}>Profile</Link>
+        <Link className={linkClasses('bookings')} to={'/account/bookings'}>My booking</Link>
+        <Link className={linkClasses('Places')} to={'/account/Places'}>My places</Link>
       </nav>
       </div>
   )
