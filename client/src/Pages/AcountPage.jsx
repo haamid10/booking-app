8b0ddf5598d8ba31e70735508268965e99052545
haamid10 <apdixamiid919@gmail.com>
2023-12-18 11:48:10 +0300

    solved again

--- client/src/Pages/AcountPage.jsx
+++ client/src/Pages/AcountPage.jsx
@@ -3,27 +3,41 @@ import { UserContext } from './../UserContext';
 import { Link, Navigate, useParams } from 'react-router-dom';
 
 const AcountPage = () => {
-
-    const {ready,user}= useContext(UserContext);
+  const {ready,user}= useContext(UserContext);
+   
+  let {subpage} = useParams();
+   
+   
     if(!ready){
       return 'loading...';
     }
     if(ready && !user) return <Navigate to='/login' />
       
-     const {subpage} = useParams();
-
+    
+    if(subpage === undefined){
+      subpage = `profile`;
+   }
      const linkClasses =(type =null)=> {
-        const baseClasses = 'py-2 px-6 ';
-        const activeClasses = 'bg-primary text-white rounded-full';
-        return type === subpage ||subpage ===undefined && type ==='profile'  ? `${baseClasses} ${activeClasses}` : baseClasses;
+        let baseClasses = 'py-2 px-6 ';
+        if(type === subpage){
+          baseClasses += 'bg-primary text-white rounded-full';
+          
+        }
+        return baseClasses;
      }
     return (
     <div>
       <nav className='w-full m-8 gap-2 justify-center flex'>
         <Link className={linkClasses('profile')} to={'/account'}>Profile</Link>
         <Link className={linkClasses('bookings')} to={'/account/bookings'}>My booking</Link>
-        <Link className={linkClasses('Places')} to={'/account/Places'}>My places</Link>
+        <Link className={linkClasses('Places')} to={'/account/Places'}>My accommodations </Link>
       </nav>
+      {subpage === 'profile' && (
+        <div className=' text-center max-w-lg mx-auto'>
+          Logged in as {user.name}({user.email}) <br/>
+          <button className='primary max-w-sm mt-2'> Logout </button>
+        </div>
+      )}
       </div>
   )
 }
