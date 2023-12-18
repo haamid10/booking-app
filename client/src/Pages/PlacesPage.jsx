c45ef152d18db0a96cfaf7ad3ac83a56965a263e
haamid10 <apdixamiid919@gmail.com>
2023-12-18 14:04:20 +0300

    Added Icons

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom"
 const PlacesPage = () => {
   return (
     <div>
-        <div>
-            <Link className=" bg-primary  text-white py-2 px-6 rounded-full" to={'/account/Places/new'}> Add New Place</Link>
+        <div className="text-center">
+            <Link className="inline-flex  bg-primary  text-white py-2 px-6 rounded-full" to={'/account/Places/new'}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+            </svg>
+            Add New Place</Link>
        
         </div>
         
