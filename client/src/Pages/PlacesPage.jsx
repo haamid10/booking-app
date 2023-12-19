143925762b2bcff36eb8f6b92de138f9afd98956
haamid10 <apdixamiid919@gmail.com>
2023-12-19 15:23:54 +0300

    magic going inside

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -3,7 +3,7 @@ const PlacesPage = () => {
   const {action} = useParams()
   // console.log( )
   return (
-    <div>
+    <div className=" w-fixed">
     {action !=='new' && (
       <div>
         <div className="text-center">
@@ -21,18 +21,25 @@ const PlacesPage = () => {
         <h3 className='text-center'>Add a new place:</h3>
         <form action="">
           <h2 className="text-xl mt-4">Title</h2>
+          <p className="text-gray-500 mt-4"> Title for your place</p>
           <input type="text" placeholder="Title" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
           <h2 className="text-xl mt-4">Address</h2>
+          <p className="text-gray-500 mt-4"> Address to this place</p>
           <input type="text" placeholder="Address" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
           <h2 className="text-xl mt-4">Photo</h2>
-          <input type="text" placeholder="Description" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Image" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Price" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Max Guests" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Phone Number" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Email" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <input type="text" placeholder="Website" className=" p-4 my-4 border border-gray-400 rounded-lg focus:outline-none focus:border-primary" />
-          <button className=" p-4 my-4 bg-primary text-white rounded-lg">Submit</button>
+          <p className="text-gray-500 mt-4"> more = better</p>
+          <div className="flex">
+            <input type="text" placeholder={'ADD using Link ....jpg'} />
+            <button className=" bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+          </div>
+          <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+          <button className="flex gap-1  border bg-transparent rounded-2xl p-4 text-md " >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+          </svg>
+
+            Upload </button>
+          </div>
 
         </form>
       </div>
