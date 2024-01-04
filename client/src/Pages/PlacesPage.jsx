0fb0369bfaf06b797b68b8cf49b7c6a1435cbc8e
haamid10 <apdixamiid919@gmail.com>
2024-01-04 15:43:39 +0300

    installing multer

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -45,6 +45,27 @@ const PlacesPage = () => {
  })
  setPhotoLink('')
 }
+const uploudPhoto = (ev) => {
+  const files = ev.target.value;
+  console.log({files})
+
+  const data=  new FormData();
+  data.set('photos', files);
+  axios.post('/upload', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  }).then(res => {
+    const {data: filename} = res;
+    setAddedPhotos(prev => {
+      return[...prev,filename]
+     })
+    console.log(data);
+  }).catch(err => console.log(err))
+  
+  
+
+}
   return (
     <div className=" w-fixed">
     {action !=='new' && (
@@ -81,7 +102,7 @@ const PlacesPage = () => {
           ))}
           
           <label className=" cursor-pointer flex gap-1  border bg-transparent rounded-2xl p-4 text-md " >
-          <input type="file" className="hidden" />
+          <input type="file" className="hidden" onChange={uploudPhoto} />
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
           </svg>
