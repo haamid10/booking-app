b1c9fe09943418ffab95a07e7a7af81959a76311
haamid10 <apdixamiid919@gmail.com>
2024-02-15 17:00:12 +0300

    succesfully fetched the data and styled

--- client/src/Pages/IndexPage.jsx
+++ client/src/Pages/IndexPage.jsx
@@ -4,23 +4,25 @@ const IndexPage = () => {
   const [places , setPlaces] = useState([])
   useEffect(() => {
     axios.get('/places').then((responce) => {
-      setPlaces(responce.data)
+      setPlaces([...responce.data,...responce.data])
     });
     
   },[])
   console.log(places)
   return (
-    <div className="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+    <div className="mt-16  grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-32 ">
       {
         places.length > 0 && places.map(place => (
-          <div className=" " key={place.id}>
+          <div key={place.id}>
              
-            
-              {place.photos?.[0]   && (
-              <img className="w-32 rounded-2xl" src={`http://localhost:5000/uploads/` + place.photos[0]} alt="" />
+            <div className="bg-gray-500 rounded-2xl flex">
+            {place.photos?.[0]   && (
+              <img className=" rounded-2xl object-cover aspect-square " src={`http://localhost:5000/uploads/` + place.photos[0]} alt="" />
             )}
-            <h1>{place.title}</h1>
-            <h1>{place.address}</h1>
+            </div>
+            
+            <h1 className="text-md">{place.title}</h1>
+            <h1 className="font-bold">{place.address}</h1>
             </div>
           // litle bit                                                     
 
