9ffe6d6c5d4e2954fd93dc2ba02c93b8f23319a1
haamid10 <apdixamiid919@gmail.com>
2024-02-18 15:39:23 +0300

    applied some edits

--- client/src/Pages/IndexPage.jsx
+++ client/src/Pages/IndexPage.jsx
@@ -10,24 +10,19 @@ const IndexPage = () => {
   },[])
   console.log(places)
   return (
-    <div className="mt-16  grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx:4 md:mx-6 lg:mx-32 ">
+    <div className="mt-16 gap-y-8 gap-x-6  grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mx:4 md:mx-6 lg:mx-32 ">
       {
         places.length > 0 && places.map(place => (
-          <div key={place.id}>
-             
-            <div className="bg-gray-500 rounded-2xl flex">
+          <div className=" " key={place.id}>
+
+            <div className="bg-gray-500 rounded-2xl flex mb-2">
             {place.photos?.[0]   && (
               <img className=" rounded-2xl object-cover aspect-square " src={`http://localhost:5000/uploads/` + place.photos[0]} alt="" />
             )}
             </div>
-            
-            <h1 className="text-md">{place.title}</h1>
-            <h1 className="font-bold">{place.address}</h1>
+            <h1 className="text-sm truncate leading-4">{place.title}</h1>
+            <h1 className="font-bold leading-4">{place.address}</h1>
             </div>
-          // litle bit                                                     
-
-            
-         
         ))
       }
     </div>
