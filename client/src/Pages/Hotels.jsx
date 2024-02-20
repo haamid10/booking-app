8c37d03e38f6b624b4acbb96eb100bd8eb0125df
haamid10 <apdixamiid919@gmail.com>
2024-02-20 16:54:50 +0300

    added

--- client/src/Pages/Hotels.jsx
+++ client/src/Pages/Hotels.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 const Hotels = () => {
     const {id} = useParams();
     const [places , setPlaces] = useState([])
+    const [showAll , setShowAll] = useState(false)
     useEffect(()=> {
         if(!id){
             return  
@@ -19,6 +20,22 @@ const Hotels = () => {
     if(!places){
         return <h1>Loading...</h1>
     }
+    if(showAll){
+        return(
+            <div className=' absolute bg-white inset-0 min-h-screen'>
+                <div>
+                    <button className=' fixed top-8 right-12  shadow-lg shadow-grey-500 bg-gray-300 py-2 px-4 rounded-2xl' onClick={()=>setShowAll(false)}>close Photos</button>
+                </div>
+            {
+                places?.photos?.length && places.photos.map(photo => (
+                    <div className=' flex flex-col justify-center items-center  object-cover p-4 '>
+                    <img className='w-[1180px]' src={`http://localhost:5000/uploads/` + photo} alt="" />
+                    </div>
+                ))
+            }</div>
+        )
+    }
+   
     // console.log(places)
   return (
     <div className='mt-4 pt-4 bg-gray-100  -mx-8 px-8'>
@@ -29,7 +46,7 @@ const Hotels = () => {
             <div>
                 {places.photos?.[0] &&(
                     <div>
-                        <img className='aspect-square object-cover' src={'http://localhost:5000/uploads/'+ places.photos[0]} alt="" />
+                        <img className='' src={'http://localhost:5000/uploads/'+ places.photos[0]} alt="" />
                     </div>
                 )}
             </div>
@@ -45,7 +62,7 @@ const Hotels = () => {
             </div>
 
         </div>
-        <button className=' absolute bottom-2 right-2  shadow-lg shadow-grey-500 bg-white py-2 px-4 rounded-2xl'>Show more photos</button>
+        <button onClick={()=> setShowAll(true)} className=' absolute bottom-2 right-2  shadow-lg shadow-grey-500 bg-white py-2 px-4 rounded-2xl'>Show more photos</button>
 
         </div>
 
