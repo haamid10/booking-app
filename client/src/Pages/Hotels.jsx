30d2c2169535f3927da8910802c023f0472cc5ed
haamid10 <apdixamiid919@gmail.com>
2024-02-20 12:19:38 +0300

    displayed the hotels in single pages

--- client/src/Pages/Hotels.jsx
+++ client/src/Pages/Hotels.jsx
@@ -22,8 +22,31 @@ const Hotels = () => {
     // console.log(places)
   return (
     <div className='mt-4 pt-4 bg-gray-100  -mx-8 px-8'>
-        <h1 className=' text-2xl'> {places.title}</h1>
-        <a target='_blank' href={'https://maps.google.com/?q='+ places.address}>{places.address}</a>
+        <h1 className=' text-3xl'> {places.title}</h1>
+        <a className='my-2 block underline font-semibold' target='_blank' href={'https://maps.google.com/?q='+ places.address}>{places.address}</a>
+        <div className="grid gap-2  grid-cols-[2fr_1fr]">
+            <div>
+                {places.photos?.[0] &&(
+                    <div>
+                        <img className='aspect-square object-cover' src={'http://localhost:5000/uploads/'+ places.photos[0]} alt="" />
+                    </div>
+                )}
+            </div>
+            <div className="grid gap-2 ">
+                {places.photos?.[1] &&(
+                        <img className='' src={'http://localhost:5000/uploads/'+ places.photos[1]} alt="" />
+                )}
+            <div>
+            {places.photos?.[2] &&(
+                        <img className='' src={'http://localhost:5000/uploads/'+ places.photos[2]} alt="" />
+                )}
+            </div>
+         
+
+               
+            </div>
+
+        </div>
 
     </div>
   )
