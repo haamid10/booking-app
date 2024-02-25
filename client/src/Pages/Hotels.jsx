b5f43b41d0ac51639391fa831cdaffa494c4a3b0
haamid10 <apdixamiid919@gmail.com>
2024-02-25 16:51:42 +0300

    designing

--- client/src/Pages/Hotels.jsx
+++ client/src/Pages/Hotels.jsx
@@ -38,7 +38,7 @@ const Hotels = () => {
    
     // console.log(places)
   return (
-    <div className='mt-4 pt-4 bg-gray-100  -mx-8 px-8'>
+    <div className='mt-4 pt-4 bg-gray-100 h-fit -mx-8 px-8'>
         <h1 className=' text-3xl'> {places.title}</h1>
         <a className='my-2 block underline font-semibold' target='_blank' href={'https://maps.google.com/?q='+ places.address}>{places.address}</a>
         <div className='relative'>
@@ -65,27 +65,52 @@ const Hotels = () => {
         <button onClick={()=> setShowAll(true)} className=' absolute bottom-2 right-2  shadow-lg shadow-grey-500 bg-white py-2 px-4 rounded-2xl'>Show more photos</button>
 
         </div>
-        <div className=' mt-8  '>
-            <div>
-                <h2 className='text-2xl'>Description</h2>
+       
+            <div className='my-4'>
+                <h2 className='font-semibold text-2xl'>Description</h2>
                 <p>{places.description}</p>
             </div>
-            <div className=' grid grid-cols-2'>
+            <div className=' grid grid-cols-[2fr_1fr]'>
+                <div>
                 Check-in {places.checkIn} <br/>
                 Check-Out {places.checkOut} <br/>
-                Max number of guests:{places.maxGuests} <br/>
-            </div>
-            <div className='bg-white  shadow p-4 rounded-2xl'>
-                <div className='text-2xl text-center'>
-                    Price: {places.price} <br/> / Per night
+                Max number of guests:{places.maxGuests} 
                 </div>
-                <button className='primary'>Book this place </button>
+               
+            
+            <div className='bg-white    shadow-xl p-4 rounded-2xl w-[400px]'>
+                <div className='text-2xl text-center'>
+                    Price: ${places.price}  / Per night
+                </div>
+                <div className="border rounded-2xl mt-4">
+                    <div className="flex">
+                        <div className='    py-3 px-4  '>
+                            <label>Check in:</label>
+                            <input type="date" />
+                        </div>
+                        <div className='     py-3 px-4 border-l'>
+                            <label>Check in:</label>
+                            <input type="date" />
+                        </div>
 
+                    </div>
+                    <div>
+                    <div className='     py-3 px-4 border-t'>
+                            <label>Number of guests:</label>
+                            <input type="number" />
+                        </div>
+                    </div>
+                
+                </div>
+               
+                <button className='primary mt-2' >Book this place </button>
+
+            </div>
             </div>
 
         </div>
 
-    </div>
+  
   )
 }
 
