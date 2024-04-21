586a490c081e6f745541eeb6209358aed0ef39a4
haamid10 <apdixamiid919@gmail.com>
2024-04-21 15:16:21 +0300

    everything working well

--- client/src/Pages/BookingWidget.jsx
+++ client/src/Pages/BookingWidget.jsx
@@ -4,10 +4,12 @@ const BookingWidget = ({places}) => {
     const [checkIn, setCheckIn] = useState('')
     const [checkOut, setCheckOut] = useState('')
     const [guests, setGuests] = useState(1)
+    const [name , setName] = useState("")
+    const [mobile , setMobile] = useState("")
 
     let numberOfNights = 0;
     if(checkIn && checkOut) {
-        numberOfNights = differenceInCalendarDays(new date(checkIn),new date(checkOut));
+        numberOfNights = differenceInCalendarDays(new Date(checkOut),new Date(checkIn));
     }
   return (
     <div>
@@ -44,11 +46,20 @@ const BookingWidget = ({places}) => {
                    </div>
                    <div>
                    <div className='     py-3 px-4 border-t'>
-                           <label>Number of guests:</label>
-                           <input type="number" value={guests} onChange={ev => setGuests(ev.target.value) }/>
-                       </div>
+                        <label>Number of guests:</label>
+                        <input type="number" value={guests} onChange={ev => setGuests(ev.target.value) }/>
+                    </div>
+                    {numberOfNights >0 && (
+                        <div className='     py-3 px-4 border-t'>
+                        <label>Enter Your Full-name:</label>
+                        <input type="number" value={name} onChange={ev => setName(ev.target.value) }/>
+                        <label>Enter Your Phone Number :</label>
+                        <input type="tele" value={mobile} onChange={ev => setMobile(ev.target.value) }/>
+                    </div>
+
+                    )}
+
                    </div>
-               
                </div>
               
             <button className='primary mt-2' >
