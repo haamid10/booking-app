71639bd7f68db9c785a00fd6a3190e7d1d232f5b
haamid10 <apdixamiid919@gmail.com>
2024-04-25 11:54:49 +0300

    I think I solved

--- server/MODELS/Booking.js
+++ server/MODELS/Booking.js
@@ -3,9 +3,9 @@ const { checkout } = require('../Routes/userRoutes');
 
 const bookingSchema = new mongoose.Schema({
     place:{type:mongoose.Schema.Types.ObjectId, ref:'Place'},
-    checkout:{type:Date, required:true},
-    checkin:{type:Date, required:true},
-    name:{type:String, required:true},
+    checkIn:{type:Date, required:true},
+    checkOut:{type:Date, required:true},
+    name:{type:String, required:true},  
     phone:{type:String, required:true},
 });
 
