8d2e1a97f3c9b60d4e12d0af7921a94c175efa47
haamid10 <apdixamiid919@gmail.com>
2024-08-14 12:20:29 +0300

    everything working well

--- client/src/Pages/BookingPage.jsx
+++ client/src/Pages/BookingPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import AccountNav from '../AccountNav'
 import axios from 'axios'
 import PlaceImg from '../components/PlaceImg';
+import { differenceInCalendarDays, format } from 'date-fns';
 const BookingPage = () => {
 
   const [bookings , setBookings] = useState('')
@@ -17,15 +18,22 @@ const BookingPage = () => {
       <AccountNav/>
       <div>
         {bookings.length > 0 && bookings.map(booking =>(
-          <div>
-            <div>
+          <div className=' flex gap-12   bg-gray-100 rounded-2xl overflow-hidden '>
+            <div className='w-48  '>
            <PlaceImg place={booking.place}/>
             </div>
-            <h2>{booking.place.title}</h2>
-            <p>Check-in: {booking.checkIn}</p>
-            <p>Check-out: {booking.checkOut}</p>
-            <p>Guests: {booking.guests}</p>
-            <p>Price: {booking.price}</p>
+            <div className=' pr-3 grow'>
+            <h2 className='text-xl font-bold'>{booking.place.title}</h2>
+            <div className='text-xl font-semibold font-sans border-t border-gray-300 mt-2 py-2 '>
+            {format(new Date(booking.checkIn), 'yyyy-MM-dd')} &rarr; 
+            {format(new Date(booking.checkOut), 'yyyy-MM-dd')} 
+
+            <p className='text-xl font-semibold font-sans'>{differenceInCalendarDays(new Date(booking.checkIn), new Date(booking.checkOut))} nights | Price: {booking.price}</p>
+
+            </div>
+            
+            </div>
+            
 
           </div>
         )) }
